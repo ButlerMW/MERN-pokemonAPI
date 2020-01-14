b5893795f009687da2fd0e9efff6e4b77ce7a917
Michael <michael_w_butler@live.com>
2020-01-13 20:38:45 -0700

    pokemon with axios

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,20 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Pokemon = () => {
-    const [state, setState] = useState("");
+    
+    let [state, setState] = useState([]);
+    let [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if (count > 0){
+            axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            // .then(response => response.json())
+            .then(response => {
+                state = response.data.results; 
+                setState([...state])}
+                );
+                // console.log(response);
+                console.log(state);  
+            }
+            },[count])
         
-    const onClickHandler = () => {  
-        console.log(state.length);
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
-        .then(response => response.json())
-        .then(response => setState(response.results))
-    };
-    console.log(state.length);
+    
+        const onClickHandler = () => {
+            console.log(state); 
+            // count++;
+            if(count == 0){
+                setCount(1);
+            }
+            else{
+                setCount(0);
+                setState([]);
+            }
+            console.log(count); 
+        };
 
     return (
         <div>
-            <button onClick={onClickHandler}>Fetch Pokemon</button>
-            {state.length > 0 && state.map((item, index) => {
+            <button onClick={onClickHandler}>{count == 0 ? "Fetch Pokemon" : "Get yo ass back here!"}</button>
+            {state.map((item, index) => {
                 return (<div key={index}>{item.name}</div>)
             })}
         </div>
